8d1682ee590a662a8b619cfa724794e5d89375b3
MariamSiad <146548903+MariamSiad@users.noreply.github.com>
2025-11-28 19:44:25 +0200

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,1 +1,18 @@
+node ('agent1'){
+    def maven = tool 'maven-352', type:'maven'
+    stage('Checkout') {
+        git branch: 'main',
+            url: 'https://github.com/Hassan-Eid-Hassan/cicd-lab2.git'
+    
+    }
+    stage('Build') {
+        sh "mvn clean package"
+    }
+    stage('Test') {
+        sh "mvn test"
+    }
+    stage('Archive') {
+        archiveArtifacts artifacts: 'target/*.jar'
+    }
+}
 
